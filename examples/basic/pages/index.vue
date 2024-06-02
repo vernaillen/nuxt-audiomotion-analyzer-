a0225a95dc73b8371b6f3a9effae7235ed91a126
Wouter Vernaillen <wouter@vernaillen.com>
2024-06-02 13:12:49 +0200

    use audio controls instead of button

--- examples/basic/pages/index.vue
+++ examples/basic/pages/index.vue
@@ -3,15 +3,11 @@
     <UContainer>
       <audio
         id="audio"
-        ref="audioRef"
         src="https://ice2.somafm.com/beatblender-128-mp3"
         crossorigin="anonymous"
-      />
-      <UButton
-        :label="isPlaying ? 'Pause' : 'Play'"
-        :icon="isPlaying ? 'i-heroicons-pause' : 'i-heroicons-play'"
+        controls
+        autoplay
         class="my-3"
-        @click="isPlaying ?audio?.pause() : audio?.play()"
       />
       <NuxtAudioMotionAnalyzer :source="audio" />
       <StreamInfo />
@@ -20,17 +16,8 @@
 </template>
 
 <script setup lang="ts">
-import { onMounted, ref } from 'vue'
-
 const audio = ref<HTMLMediaElement>()
-const isPlaying = ref(false)
 onMounted(() => {
   audio.value = document.getElementById('audio') as HTMLMediaElement
-  audio.value.onplaying = () => {
-    isPlaying.value = true
-  }
-  audio.value.onpause = () => {
-    isPlaying.value = false
-  }
 })
 </script>
