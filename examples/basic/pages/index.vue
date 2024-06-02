77585585c3a3618ab41c0a9659c8c16063856f3e
Wouter Vernaillen <wouter@vernaillen.com>
2024-06-02 13:30:17 +0200

    revert controls

--- examples/basic/pages/index.vue
+++ examples/basic/pages/index.vue
@@ -1,12 +1,17 @@
 <template>
   <UPage>
     <UContainer>
+      <UButton
+        :label="isPlaying ? 'Pause' : 'Play'"
+        :icon="isPlaying ? 'i-heroicons-pause' : 'i-heroicons-play'"
+        class="my-3"
+        @click="isPlaying ? audio?.pause() : audio?.play()"
+      />
       <audio
         id="audio"
+        ref="audioRef"
         src="https://ice2.somafm.com/beatblender-128-mp3"
         crossorigin="anonymous"
-        controls
-        autoplay
         class="my-3"
       />
       <NuxtAudioMotionAnalyzer :source="audio" />
@@ -16,8 +21,17 @@
 </template>
 
 <script setup lang="ts">
+import { onMounted, ref } from 'vue'
+
 const audio = ref<HTMLMediaElement>()
+const isPlaying = ref(false)
 onMounted(() => {
   audio.value = document.getElementById('audio') as HTMLMediaElement
+  audio.value.onplaying = () => {
+    isPlaying.value = true
+  }
+  audio.value.onpause = () => {
+    isPlaying.value = false
+  }
 })
 </script>
